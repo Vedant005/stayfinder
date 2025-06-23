1d2a32220b39bf73b4248ae925ac8a5265679611
Vedant005 <vedantkanekar909@gmail.com>
2025-06-23 20:28:44 +0530

    feat: add routing for authentication, listings, and bookings

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,4 +14,12 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+import authRouter from "./routes/user.js";
+import listingRouter from "./routes/listing.js";
+import bookingRouter from "./routes/booking.js";
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/listings", listingRouter);
+app.use("/api/v1/bookings", bookingRouter);
+
 export { app };
